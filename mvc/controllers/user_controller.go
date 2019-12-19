15529dcebf95a8d48a8b8650182199fc2a1fad9e
Chaiwat Tungtongsoontorn <nick@Nicks-macbook.local>
2019-12-19 23:38:03 +0700

    [master] refactor code

--- mvc/controllers/user_controller.go
+++ mvc/controllers/user_controller.go
@@ -3,9 +3,15 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func GetUsers(res http.ResponseWriter, req *http.Request) {
-	userId := req.URL.Query().Get("userId")
-	res.Write([]byte(fmt.Sprintf("User Id: %s", userId)))
+	userId, err := strconv.ParseInt(req.URL.Query().Get("userId"), 10, 64)
+	if err != nil {
+		res.Write([]byte("Error woy"))
+		return
+	}
+
+	res.Write([]byte(fmt.Sprintf("User Id naja: %d", userId)))
 }
